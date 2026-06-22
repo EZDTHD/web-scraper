e89dc6540a9ac774fae902648a47f947dba8ee7d
Tarek Ballita <tarek08ballita@gmail.com>
2026-06-22 17:44:28 +0200

    hep

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+response = requests.get("https://www.gelbeseiten.de/suche/automobile/soest")
+soup = BeautifulSoup(response.text, "html.parser")
+title = soup.find_all("h2", class_="mod-Treffer__name")
+nummer = soup.find_all("a", class_="mod-TelefonnummerKompakt__phoneNumber")
+with open("info.csv", "w")as f:
+    for i in title:
+        f.write(i.text.strip() + "\n")
+    for i in nummer:
+        f.write(i.text.strip()+ "\n")

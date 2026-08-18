fe076f532c580931e36f48d48acfb19c48e166b8
Tarek <125267793+EZDTHD@users.noreply.github.com>
2026-08-18 11:56:02 +0200

    security change for link
    Updated URL for web scraping from Gelbe Seiten to Quotes to Scrape.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.gelbeseiten.de/suche/automobile/soest")
+response = requests.get("https://quotes.toscrape.com")
 soup = BeautifulSoup(response.text, "html.parser")
 title = soup.find_all("h2", class_="mod-Treffer__name")
 nummer = soup.find_all("a", class_="mod-TelefonnummerKompakt__phoneNumber")
